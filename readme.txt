f6c3ab295e67485848f32729a40d151552db8f9d
Pengfeiworld <131347949+Pengfeiworld@users.noreply.github.com>
2023-04-20 16:45:28 +0800

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,4 @@
+Name of the code: theorical_solution
+manuscript:Three-Dimensional Analytical Solution of Self-potential from Regularly Polarized Bodies in Layered Seafloor Model
+Devoloper: Pengfei Zhang E-mail:symdwjz@foxmail.com
+The data include the background and the anomaly data.
---
+++ theorical_solution.m
@@ -0,0 +1,91 @@
+%%%%% 镜像法求解电偶极子电位 %%%%%  
+% a0           %空气电导率
+% b0           %空气电容率
+a1=81;           %海水电容率
+b1=4;           %海水电导率
+% a2=           %硫化物电容率
+b2=0.04;           %硫化物电导率
+h=50;         %海水深度
+d=50;         %海床深度
+x0=0;         %源点位置 X
+y0=0;         %源点位置 Y
+z0=50;        %源点位置 Z
+Ix=-1;
+Iy=0;
+Iz=0;         %电偶极矩
+x=-100:2.5:100;
+y=-100:2.5:100;   
+z=-10;  %测线范围
+%% 
+for i=1:length(x)     %行
+    for j=1:length(y) %列
+        for ii=1:length(z)
+            n=(b1-b2)/(b1+b2);
+            r1m(1,i,j,ii)=((x(i)-x0)^2+(y(j)-y0)^2+(z(ii)+2*(1-1)*h-z0)^2)^(1/2);
+            r2m(1,i,j,ii)=((x(i)-x0)^2+(y(j)-y0)^2+(z(ii)+2*(1-1)*h+z0)^2)^(1/2);
+            r1k(1,i,j,ii)=((x(i)-x0)^2+(y(j)-y0)^2+(z(ii)-2*(1)*h+z0)^2)^(1/2);
+            r2k(1,i,j,ii)=((x(i)-x0)^2+(y(j)-y0)^2+(z(ii)-2*(1)*h-z0)^2)^(1/2);
+            sum_x_k(1,i,j,ii)=((n^1*Ix*(x(i)-x0)/(4*pi*b1*r1k(1,i,j,ii)^3))+(n^1*Ix*(x(i)-x0)/(4*pi*b1*r2k(1,i,j,ii)^3)));
+            sum_x_m(1,i,j,ii)=((n^0*Ix*(x(i)-x0)/(4*pi*b1*r1m(1,i,j,ii)^3))+(n^0*Ix*(x(i)-x0)/(4*pi*b1*r2m(1,i,j,ii)^3)));
+            sum_y_k(1,i,j,ii)=((n^1*Iy*(y(j)-y0)/(4*pi*b1*r1k(1,i,j,ii)^3))+(n^1*Iy*(y(j)-y0)/(4*pi*b1*r2k(1,i,j,ii)^3)));
+            sum_y_m(1,i,j,ii)=((n^0*Iy*(y(j)-y0)/(4*pi*b1*r1m(1,i,j,ii)^3))+(n^0*Iy*(y(j)-y0)/(4*pi*b1*r2m(1,i,j,ii)^3)));
+            sum_z_k(1,i,j,ii)=-((n^1*Iz*(z(ii)-2*1*h+z0)/(4*pi*b1*r1k(1,i,j,ii)^3))+(n^1*Iz*(z(ii)-2*1*h-z0)/(4*pi*b1*r2k(1,i,j,ii)^3)));
+            sum_z_m(1,i,j,ii)=((n^0*Iz*(z(ii)+2*0*h-z0)/(4*pi*b1*r1m(1,i,j,ii)^3))-(n^0*Iz*(z(ii)+2*0*h+z0)/(4*pi*b1*r2m(1,i,j,ii)^3)));
+            ux(1,i,j,ii)=sum_x_k(1,i,j,ii)+sum_x_m(1,i,j,ii);
+            uy(1,i,j,ii)=sum_y_k(1,i,j,ii)+sum_y_m(1,i,j,ii);
+            uz(1,i,j,ii)=sum_z_k(1,i,j,ii)+sum_z_m(1,i,j,ii);
+              for k=2:10
+                  for m=2:11
+                      r1m(m,i,j,ii)=((x(i)-x0)^2+(y(j)-y0)^2+(z(ii)+2*(m-1)*h-z0)^2)^(1/2);
+                      r2m(m,i,j,ii)=((x(i)-x0)^2+(y(j)-y0)^2+(z(ii)+2*(m-1)*h+z0)^2)^(1/2);
+                      r1k(k,i,j,ii)=((x(i)-x0)^2+(y(j)-y0)^2+(z(ii)-2*(k)*h+z0)^2)^(1/2);
+                      r2k(k,i,j,ii)=((x(i)-x0)^2+(y(j)-y0)^2+(z(ii)-2*(k)*h-z0)^2)^(1/2);
+                      
+                      ux_k(k,i,j,ii)=((n^k*Ix*(x(i)-x0)/(4*pi*b1*r1k(k,i,j,ii)^3))+(n^k*Ix*(x(i)-x0)/(4*pi*b1*r2k(k,i,j,ii)^3)));
+                      sum_x_k(k,i,j,ii)=ux_k(k,i,j,ii)+sum_x_k(k-1,i,j,ii);
+                      uy_k(k,i,j,ii)=((n^k*Iy*(y(j)-y0)/(4*pi*b1*r1k(k,i,j,ii)^3))+(n^k*Iy*(y(j)-y0)/(4*pi*b1*r2k(k,i,j,ii)^3)));
+                      sum_y_k(k,i,j,ii)=uy_k(k,i,j,ii)+sum_y_k(k-1,i,j,ii);
+                      uz_k(k,i,j,ii)=-((n^k*Iz*(z(ii)-2*k*h+z0)/(4*pi*b1*r1k(k,i,j,ii)^3))+(n^k*Iz*(z(ii)-2*k*h-z0)/(4*pi*b1*r2k(k,i,j,ii)^3)));
+                      sum_z_k(k,i,j,ii)=uz_k(k,i,j,ii)+sum_z_k(k-1,i,j,ii);
+                      
+                      ux_m(m,i,j,ii)=((n^(m-1)*Ix*(x(i)-x0)/(4*pi*b1*r1m(m,i,j,ii)^3))+(n^(m-1)*Ix*(x(i)-x0)/(4*pi*b1*r2m(m,i,j,ii)^3)));
+                      sum_x_m(m,i,j,ii)=ux_m(m,i,j,ii)+sum_x_m(m-1,i,j,ii);
+                      uy_m(m,i,j,ii)=((n^(m-1)*Iy*(y(j)-y0)/(4*pi*b1*r1m(m,i,j,ii)^3))+(n^(m-1)*Iy*(y(j)-y0)/(4*pi*b1*r2m(m,i,j,ii)^3)));
+                      sum_y_m(m,i,j,ii)=uy_m(m,i,j,ii)+sum_y_m(m-1,i,j,ii);
+                      uz_m(m,i,j,ii)=((n^(m-1)*Iz*(z(ii)+2*m*h-z0)/(4*pi*b1*r1m(m,i,j,ii)^3))-(n^(m-1)*Iz*(z(ii)+2*m*h+z0)/(4*pi*b1*r2m(m,i,j,ii)^3)));
+                      sum_z_m(m,i,j,ii)=uz_m(m,i,j,ii)+sum_z_m(m-1,i,j,ii);
+                      
+                      end_k=max(k);
+                      end_m=max(m);
+                      end_sum_x_k(i,j,ii)=sum_x_k(end_k,i,j,ii);
+                      end_sum_x_m(i,j,ii)=sum_x_m(end_m,i,j,ii);
+                      end_sum_y_k(i,j,ii)=sum_y_k(end_k,i,j,ii);
+                      end_sum_y_m(i,j,ii)=sum_y_m(end_m,i,j,ii);
+                      end_sum_z_k(i,j,ii)=sum_z_k(end_k,i,j,ii);
+                      end_sum_z_m(i,j,ii)=sum_z_m(end_m,i,j,ii);
+                  end
+              end
+             ux=end_sum_x_k+end_sum_x_m;
+             uy=end_sum_y_k+end_sum_y_m;
+             uz=end_sum_z_k+end_sum_z_m;
+             u=ux+uy+uz;
+
+             
+        end
+    end
+end
+%% 数据加密、绘图和导出
+%XX=result(:,2);YY=result(:,3);UU=result(:,1);
+[XX1,YY1]=meshgrid(-100:2.5:100,-100:2.5:100);
+[XX2,YY2]=meshgrid(-100:1:100,-100:1:100);
+u2=interp2(XX1,YY1,u,XX2,YY2,'spline');
+tri=delaunay(XX2,YY2); 
+trisurf(tri,XX2,YY2,u2);
+shading interp
+% contourf(XX2,YY2,u2,10);
+c=[];
+result=[];
+
+
+
+
